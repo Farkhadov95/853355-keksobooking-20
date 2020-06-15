d89dfff519cdbfcb997193fdc50dff978774747b
Amir Farkhadov <Farkhadov95@mail.ru>
2020-06-15 20:03:17 +0500

    Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@
 (function () {
   var fragment = document.createDocumentFragment();
   var pinContainer = document.querySelector('.map__pins');
-
-  function showMap() {
-    var map = document.querySelector('.map');
-    map.classList.remove('map--faded');
-  }
-  showMap();
+  document.querySelector('.map').classList.remove('map--faded');
 
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -25,10 +20,15 @@
     return photoLink;
   }
 
-  // function getRandomLocation(placement, shift, min, max) {
-  //   var randomLocation = placement + (getRandomIntInclusive(min, max) - shift) + 'px; ';
-  //   return randomLocation;
-  // }
+  function getRandomLocationX() {
+    var locationX = getRandomIntInclusive(25, 1175);
+    return locationX;
+  }
+
+  function getRandomLocationY() {
+    var locationY = getRandomIntInclusive(130, 630);
+    return locationY;
+  }
 
   function getRandomElement(item) {
     var randomElement = item[Math.floor(Math.random() * item.length)];
@@ -47,11 +47,53 @@
     fragment.appendChild(pinElement);
   }
 
+
+  function renderCards(card) {
+    var map = document.querySelector('.map');
+
+    var mapFilter = document.querySelector('.map__filters-container');
+    var offerTemplate = document.querySelector('#card').content;
+    var offerElement = offerTemplate.cloneNode(true);
+
+    var popupTitle = offerElement.querySelector('.popup__title');
+    popupTitle.textContent = card.offer.title;
+
+    var popupAddress = offerElement.querySelector('.popup__text--address');
+    popupAddress.textContent = card.offer.address;
+
+    var popupPrice = offerElement.querySelector('.popup__text--price');
+    popupPrice.textContent = card.offer.price + ' ₽/ночь';
+
+    var popupType = offerElement.querySelector('.popup__type');
+    popupType.textContent = card.offer.type;
+
+    var popupCapacity = offerElement.querySelector('.popup__text--capacity');
+    popupCapacity.textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests;
+
+    var popupTime = offerElement.querySelector('.popup__text--time');
+    popupTime.textContent = 'Заезд после ' + card.offer.checkin + ', выезд до' + card.offer.checkout;
+
+    var popupFeatures = offerElement.querySelector('.popup__features');
+    popupFeatures.textContent = card.offer.features;
+
+    var popupDescription = offerElement.querySelector('.popup__description');
+    popupDescription.textContent = card.offer.description;
+
+    var popupPhotos = offerElement.querySelector('.popup__photos');
+    var popupPhoto = popupPhotos.querySelector('.popup__photo');
+    popupPhoto.src = card.offer.photos;
+
+    var popupAvatar = offerElement.querySelector('.popup__avatar');
+    popupAvatar.src = card.author.avatar;
+    map.insertBefore(offerElement, mapFilter);
+  }
+
+
   function displayPoints() {
-    var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+    var TYPES = ['Дворец ', 'Квартира ', 'Дом ', 'Бунгало'];
     var TIME_LIST = ['12:00', '13:00', '14:00'];
-    var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-    var PHRASE_MIX = ['Квартира с красивым видом', 'Квартира с большой кухней', 'Квартира в центре Токио', 'Квартира рядом с метро', 'Квартира со всеми удобствами', 'Квартира на тихой улице'];
+    var FEATURES = [' wifi', ' dishwasher', ' parking', ' washer', ' elevator', ' conditioner'];
+    var PHRASE_MIX = ['Красивый вид', 'Большая кухня', 'В центре Токио', 'Рядом с метро', 'Со всеми удобствами', 'На тихой улице'];
 
 
     for (var i = 0; i < 8; i++) {
@@ -61,23 +103,24 @@
         },
         offer: {
           title: getRandomElement(PHRASE_MIX),
-          address: location.x + location.y,
-          price: getRandomIntInclusive(25, 250),
+          address: getRandomLocationX() + ', ' + getRandomLocationY(),
+          price: getRandomIntInclusive(25, 10000),
           type: getRandomElement(TYPES),
-          rooms: getRandomIntInclusive(1, 4),
-          guests: getRandomIntInclusive(1, 4),
+          rooms: getRandomIntInclusive(2, 4),
+          guests: getRandomIntInclusive(2, 4),
           checkin: getRandomElement(TIME_LIST),
           checkout: getRandomElement(TIME_LIST),
-          features: getRandomElement(FEATURES),
+          features: FEATURES,
           description: getRandomElement(PHRASE_MIX) + ', ' + getRandomElement(PHRASE_MIX),
           photos: getRandomPhoto()
         },
         location: {
-          x: getRandomIntInclusive(25, 1175),
-          y: getRandomIntInclusive(130, 630)
+          x: getRandomLocationX(),
+          y: getRandomLocationY()
         }
       };
       renderPins(pinPoint);
+      renderCards(pinPoint);
     }
     pinContainer.appendChild(fragment);
   }
